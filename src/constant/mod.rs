c9ba1d921b92505c134100e7e6c7579e95e11239
wyfssky <wyfssky@gmail.com>
2022-06-19 18:06:12 +0800

    update callback_url

--- src/constant/mod.rs
+++ src/constant/mod.rs
@@ -1,12 +1,13 @@
 pub const MYSQL_URL: &'static str = "mysql://nft:nft@101.33.60.164:3306/nft";
 pub const KEY: &[u8] = b"secretzero-labNifties002233hahahaa";
-pub const MAIN_URL: &'static str = "http://101.33.60.164/";
+//pub const MAIN_URL: &'static str = "http://101.33.60.164/";
+pub const MAIN_URL: &'static str = "https://api.niftiesx.xyz/";
 //pub const MAIN_URL: &'static str = "http://127.0.0.1:8888/";
 pub const CONSUMER_KEY: &'static str = "2jQ0ahrYN7SJZu8eQZlfbaaIB";
 pub const CONSUMER_SECRET: &'static str = "N93cynzXCBU4xkUk0hc92Yhp8ajreCqyWdtn2bR295tzdk3YPA";
 
 //twitter
-pub const OAUTH_CALLBACK: &'static str = "http://101.33.60.164/gettoken";
+pub const OAUTH_CALLBACK: &'static str = "https://www.niftiesx.xyz/#/authPage";
 pub const AUTHORIZE_URL: &'static str = "https://api.twitter.com/oauth/authorize";
 pub const ACCESS_TOKEN: &'static str = "https://api.twitter.com/oauth/access_token";
 pub const TWEET: &'static str = "https://api.twitter.com/2/tweets";
